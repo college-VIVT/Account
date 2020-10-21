ccdb29ad4c29ab6a28941237247ccf35fbcbe3fb
Starinin Andrey <starininandrey@gmail.com>
2020-10-21 09:47:34 +0300

    Update AccountModelData/ModelData.cs
    Co-authored-by: NinelNina <61094105+NinelNina@users.noreply.github.com>

--- AccountModelData/ModelData.cs
+++ AccountModelData/ModelData.cs
@@ -11,7 +11,7 @@ namespace AccountModelData
     
     public class User
     {
-        public int ID { get; set; }
+        public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
